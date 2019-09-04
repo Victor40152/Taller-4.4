25702838c197b45c7f41a4f0949b90bff7b68164
Victor Manuel Echavarria <vicme93@gmail.com>
2019-09-03 20:30:22 -0500

    Taller de cambiar nombre a color

---
+++ taller4.3/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace taller4._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.btnenviar = new System.Windows.Forms.Button();
+            this.lblnom = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(234, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Programa de Nombres";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese su Nombre :";
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnombre.Location = new System.Drawing.Point(344, 313);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(0, 24);
+            this.lblnombre.TabIndex = 2;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(325, 155);
+            this.txtnombre.Multiline = true;
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(159, 27);
+            this.txtnombre.TabIndex = 3;
+            // 
+            // btnenviar
+            // 
+            this.btnenviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnenviar.Location = new System.Drawing.Point(534, 158);
+            this.btnenviar.Name = "btnenviar";
+            this.btnenviar.Size = new System.Drawing.Size(107, 27);
+            this.btnenviar.TabIndex = 4;
+            this.btnenviar.Text = "Enviar";
+            this.btnenviar.UseVisualStyleBackColor = true;
+            this.btnenviar.Click += new System.EventHandler(this.btnenviar_Click);
+            // 
+            // lblnom
+            // 
+            this.lblnom.AutoSize = true;
+            this.lblnom.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnom.Location = new System.Drawing.Point(365, 285);
+            this.lblnom.Name = "lblnom";
+            this.lblnom.Size = new System.Drawing.Size(52, 24);
+            this.lblnom.TabIndex = 5;
+            this.lblnom.Text = "------";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblnom);
+            this.Controls.Add(this.btnenviar);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Button btnenviar;
+        private System.Windows.Forms.Label lblnom;
+    }
+}
+
---
+++ taller4.3/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taller4._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnenviar_Click(object sender, EventArgs e)
+        {
+            lblnom.Text = txtnombre.Text;
+            lblnom.ForeColor = Color.Blue;
+            lblnom.Font = new Font(lblnom.Font.Name, 25);
+            lblnom.Font = new System.Drawing.Font(lblnom.Font, FontStyle.Bold);
+
+
+
+        }
+    }
+}
